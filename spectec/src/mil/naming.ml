ea03c3f0607cc290f2c2c413afc4028245addba2
DCupello1 <dc1020@ic.ac.uk>
2025-07-12 12:41:04 +0200

    Improved names of function parameters

--- spectec/src/mil/naming.ml
+++ spectec/src/mil/naming.ml
@@ -1,9 +1,10 @@
 open Util.Source
 open Ast
 open Translate
-(* TODO add this later *)
+
+
 (* Generates a fresh name if necessary, and goes up to a maximum which then it will return an error*)
-(* let generate_var at ids i =
+let generate_var at ids i =
   (* TODO - could make these variables a record type to make it configurable *) 
   let start = 0 in
   let fresh_prefix = "var" in
@@ -17,31 +18,21 @@ open Translate
   in
   match i with
     | "" | "_" -> go fresh_prefix start
+    | s when (s = var_prefix ^ "_") -> go fresh_prefix start
     | s when List.mem s ids -> go i start
     | _ -> i
 
-let improve_ids_params params = 
-  let get_id p = match p.it with
-    | ExpP (id, _) -> id.it
-    | TypP id -> id.it
-    | DefP (id, _, _) -> id.it
-    | GramP (id, _) -> id.it
-  in
-  let construct_with_name p name = (match p.it with 
-    | ExpP (id, typ) -> ExpP(name $ id.at, typ)
-    | TypP id -> TypP (name $ id.at)
-    | DefP (id, params, typ) -> DefP(name $ id.at, params, typ)
-    | GramP (id, typ) -> GramP (name $ id.at, typ)) $ p.at
-  in
-  let rec improve_ids_helper ids ps = 
-    match ps with
+(* NOTE - This only works for function defs binders since they are not used later.
+  Would need to make a subst for MIL to make this work for all binders *)
+let improve_ids_binders at binders = 
+  let rec improve_ids_helper ids bs = 
+    match bs with
       | [] -> []
-      | p :: ps' -> 
-        let p_id = get_id p in 
-        let new_name = generate_var p.at ids p_id in 
-        construct_with_name p new_name :: improve_ids_helper (new_name :: ids) ps'
+      | (b_id, t) :: bs' -> 
+        let new_name = generate_var at ids b_id in 
+        (new_name, t) :: improve_ids_helper (new_name :: ids) bs'
   in
-  improve_ids_helper [] params *)
+  improve_ids_helper [] binders
 
 (* Can only be used for user defined types*)
 
@@ -170,7 +161,7 @@ let rec transform_def prefix_map (d : mil_def) =
       (new_id, transform_binders prefix_map bs)
     ) entries)
   | MutualRecD defs -> MutualRecD (List.map (transform_def prefix_map) defs)
-  | DefinitionD (id, bs, rt, clauses) -> DefinitionD (id, transform_binders prefix_map bs, transform_type prefix_map rt,
+  | DefinitionD (id, bs, rt, clauses) -> DefinitionD (id, transform_binders prefix_map (improve_ids_binders d.at bs), transform_type prefix_map rt,
     List.map (fun (ts, fb) -> (List.map (transform_term prefix_map) ts, transform_fb prefix_map fb)) clauses)
   | GlobalDeclarationD (id, rt, (ts, fb)) -> GlobalDeclarationD (id, transform_type prefix_map rt, (List.map (transform_term prefix_map) ts, transform_fb prefix_map fb))
   | InductiveRelationD (id, types, entries) -> 
@@ -184,7 +175,7 @@ let rec transform_def prefix_map (d : mil_def) =
         List.map (transform_premise prefix_map) prems, 
         List.map (transform_term prefix_map) terms)
     ) entries)
-  | AxiomD (id, bs, rt) -> AxiomD (id, transform_binders prefix_map bs, transform_type prefix_map rt)
+  | AxiomD (id, bs, rt) -> AxiomD (id, transform_binders prefix_map (improve_ids_binders d.at bs), transform_type prefix_map rt)
   | InductiveFamilyD (id, bs, entries) -> InductiveFamilyD (id, transform_binders prefix_map bs, 
     List.map (fun (terms, t) -> (List.map (transform_term prefix_map) terms, transform_term prefix_map t)) entries)
   | CoercionD (id1, id2, id3) -> CoercionD (id1, id2, id3)
